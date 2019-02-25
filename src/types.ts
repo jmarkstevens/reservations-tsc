a208928e0f6dd672c1c2de8b3e77474e6c5e18ba
J. Mark Stevens <github@jmarkstevens.com>
2019-02-25 13:17:33 -0800

    shared type definitions

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+
+interface Reservation {
+  name: string,
+  hotelName: string,
+  arrivalDate: string,
+  departureDate: string,
+}
+
+interface MutateVariables {
+  variables: Reservation
+}
+
+export interface MutationProp{
+  mutate: (({}: MutateVariables) => Promise<void>),
+}
+
+export interface MutationPropNull{
+  mutate: (({}: MutateVariables) => Promise<void>) | null,
+}
+
+export interface ReservationResponse {
+  id: string,
+  name: string,
+  hotelName: string,
+  arrivalDate: string,
+  departureDate: string,
+}
+
+export interface ResevationListProps { data: { refetch: () => void, reservations: ReservationResponse[] } }
